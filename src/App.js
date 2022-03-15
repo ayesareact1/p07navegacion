5848300ae1d1788aabbb82f411d4ef5b949a992f
ayesareact1 <ayesareact1@gmail.com>
2022-03-15 16:50:18 +0100

    guard

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React, { useEffect } from "react";
-import { Link, Route, Routes } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, Route, Routes, useNavigate } from "react-router-dom";
 import NavMenu from "./components/NavMenu";
+import ProtectRoute from "./guards/ProtectRoute";
+import AccesoNoPermitido from "./pages/AccesoNoPermitido";
 import Inicio from "./pages/Inicio";
 import Soporte from "./pages/Soporte";
 
@@ -12,24 +14,41 @@ const InicioRRHH = React.lazy(() => import('./modules/rrhh/InicioRRHH'));
 
 function App() {
 
+  // Simulación o mock de un sistema de autenticación
+
+  const [usuario, setUsuario] = useState(null);
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    setUsuario({nombre: 'Juan Pérez', rol: 'ventas'});
+    navigate('/');
+  }
+
+  const handleLogout = () => {
+    setUsuario(null);
+    navigate('/');
+  }
+
   return (
     <>
-      <NavMenu />
+      <NavMenu usuario={usuario} handleLogin={handleLogin} handleLogout={handleLogout} />
       <Routes>
         <Route path="/" element={<Inicio />} /> {/* La correspondiente a la ruta base del dominio*/}
         <Route path="soporte" element={<Soporte />} />
         {/* Ruta para módulo lazy loading */}
         <Route path="/ventas/*" element={
-          <React.Suspense fallback={<p style={{textAlign: 'center'}}>Cargando...</p>}>
-            <InicioVentas />
-         </React.Suspense>
+          <ProtectRoute usuario={usuario}>
+            <React.Suspense fallback={<p style={{textAlign: 'center'}}>Cargando...</p>}>
+              <InicioVentas />
+            </React.Suspense>
+          </ProtectRoute>
         } />
         <Route path="/recursos-humanos/*" element={
           <React.Suspense fallback={<p style={{textAlign: 'center'}}>Cargando...</p>}>
             <InicioRRHH />
          </React.Suspense>
         } />
-
+        <Route path="no-permitido" element={<AccesoNoPermitido />}/>
         {/* Para las rutas no existentes usamos máscara * al final */}
         <Route path="*" element={
           <div className="container">
